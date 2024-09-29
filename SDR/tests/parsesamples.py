4d147b750797f4aabb99efd5350cde7c6afa7be0
rylanpaul <rylan@ascentxr.com>
2024-09-29 16:42:03 -0400

    small update to parsesamples.py

--- SDR/tests/parsesamples.py
+++ SDR/tests/parsesamples.py
@@ -73,7 +73,7 @@ def plot_iq_data_and_frequency(iq_data, sampling_rate):
 
 # Usage example
 filename = 'output.a1'  # Replace with the actual filename
-sampling_rate = 850000000  # Replace with the actual sampling rate in Hz
+sampling_rate = 4000000000  # Replace with the actual sampling rate in Hz
 
 
 # Read the I/Q data from the file
@@ -81,41 +81,3 @@ iq_data = read_iq_data(filename)
 
 # Plot the time domain (I/Q) and frequency domain
 plot_iq_data_and_frequency(iq_data, sampling_rate)
-
-fig = plt.figure(figsize=(12, 8))
-graph = fig.add_subplot(221, projection='3d')
-
-real_parts = np.real(iq_data)
-imaginary_parts = np.imag(iq_data)
-indices = np.arange(len(iq_data))
-
-live_real = real_parts[0] # real_parts
-live_imaj = imaginary_parts[0] # imaginary_parts
-live_indices = indices[0] # indices
-
-# Second subplot: Real, Imaginary, and Index (3D Scatter Plot)
-graph = plt.scatter(live_indices, live_real, live_imaj, color='red')
-plt.pause(1)
-indices = np.arange(len(iq_data))  # Indices along the x-axis
-
-#graph.set_title('I and Q vs Index')
-# graph.set_xlabel('Index')
-# graph.set_ylabel('Real Part (I)')
-# graph.set_zlabel('Imaginary Part (Q)')
-# graph.grid(True)
-
-
-index = 0
-while(True):
-    live_real = real_parts[:index] # real_parts
-    live_imaj = imaginary_parts[:index] # imaginary_parts
-    live_indices = indices[:index] # indices
-    graph.remove()
-    graph = plt.scatter(live_indices, live_real, live_imaj, color='red')
-    # graph.xlim(live_indices[0], live_indices[index])
-    # graph.ylim(live_real[0], live_real[index])
-    # graph.zlim(live_imaj[0], live_imaj[index])
-    index += 10000
-    if (index >= len(indices)):
-        index = len(indices) - 1
-    plt.pause(0.001)
