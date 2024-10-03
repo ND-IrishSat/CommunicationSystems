c5f3af16906e979778ee3c43a6e7c43b4de55795
NDIrishSat <irishsat@nd.edu>
2024-10-03 17:39:58 -0400

    receive.py updated

--- USRP/receive.py
+++ USRP/receive.py
@@ -8,5 +8,11 @@ freq = 4218274940
 sample_rate = 1e6
 gain = 20
 samples = usrp.recv_num_samps(num_samps, freq, sample_rate, [0], gain) # units: N, Hz, list of channel IDs, dB
-print(samples[0:10])
+
+samples_str = '\n'.join(map(str,samples))
+
+output_file = "samples_output.txt"
+with open(output_file, "w") as f:
+    f.write(samples_str)
+print(f"Samples written to {output_file}")
 
