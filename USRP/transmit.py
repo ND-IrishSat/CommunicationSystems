61721cff57d34f1d8ae111f867c183f98fc2370f
rylanpaul <rylan@ascentxr.com>
2024-11-08 15:20:36 -0500

    chisum talk

--- USRP/transmit.py
+++ USRP/transmit.py
@@ -7,6 +7,6 @@ time = np.arange(0,1,1.0/10000)
 msg_freq = 3000000 # 2MHz 
 samples = np.sin(2.0 * np.pi * msg_freq * time) # create random signal
 duration = 10 # seconds
-center_freq = 418274940 # Hz
+center_freq = 428274940 # Hz
 gain = 50 # [dB] start low then work your way up
 usrp.send_waveform(samples, duration, center_freq, sample_rate, [0], gain)
